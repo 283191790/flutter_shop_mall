a324b35c1f189a3fe89a4486028629cd4e66ff82
28311790 <87002233@qq.com>
2019-10-28 00:14:43 +0800

    push

---
+++ lib/config/color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class KColor{
+
+  static const Color primaryColor = Color.fromRGBO(132, 95, 63, 1.0);//默认主要颜色
+  static const Color refreshTextColor = Color.fromRGBO(132, 95, 63, 1.0);//刷新文本颜色
+
+}
---
+++ lib/config/string.dart
@@ -0,0 +1,5 @@
+class KString{
+  static const String loading = '加载中';
+  static const String loadReadyText = '上拉加载';
+
+}
